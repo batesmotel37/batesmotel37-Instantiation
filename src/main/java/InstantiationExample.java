cb0f6fe45f8f7637676bef27e237df222247083a
Kenneth High <kennethhigh7@gmail.com>
2023-01-28 10:41:41 +0000

    commit Instantiation Example Lab

--- src/main/java/InstantiationExample.java
+++ src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object Obj = new Object();
+        return Obj;
     }
 }
